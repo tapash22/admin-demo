b2e1982525774424fb600b56ecc8da8245e41412
tapash22 <tapasp263@gmail.com>
2022-09-28 20:14:56 +0600

    add new page

---
+++ src/views/forms/RegisterForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <h2 class="grey lighten-5 pa-5">Registration</h2>
+        <BaseForm :registrationForm='registrationForm' />
+    </v-container>
+</template>
+
+<script>
+import BaseForm from './BaseForm.vue';
+
+export default {
+    data() {
+        return {
+            registrationForm: {
+                name: '',
+                email: '',
+                file: '',
+                description: ''
+            }
+        }
+    },
+    components: { BaseForm }
+}
+</script>
